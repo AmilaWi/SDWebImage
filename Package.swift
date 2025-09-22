58e032835bb1b3e489602faea0ec56d0202fbb33
AmilaWi <amila.wijayarathna@pickme.lk>
2025-09-22 14:58:55 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,23 +12,18 @@ let package = Package(
         .watchOS(.v2)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SDWebImage",
-            targets: ["SDWebImage"]),
+            name: "SDWebImageCore2",
+            targets: ["SDWebImageCore2"]),
         .library(
             name: "SDWebImageMapKit",
             targets: ["SDWebImageMapKit"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SDWebImage",
+            name: "SDWebImageCore2",
             dependencies: [],
             path: "SDWebImage",
             sources: ["Core", "Private"],
@@ -40,7 +35,7 @@ let package = Package(
         ),
         .target(
             name: "SDWebImageMapKit",
-            dependencies: ["SDWebImage"],
+            dependencies: ["SDWebImageCore2"],
             path: "SDWebImageMapKit",
             sources: ["MapKit"],
             resources: [.copy("Resources/PrivacyInfo.xcprivacy")]
